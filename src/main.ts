2c581dc34edcd87793609766f7620d541dd6d282
MicheleTodeschini <micheletodeschini01@gmail.com>
2026-04-22 14:19:38 +0200

    solved milestone 3

---
+++ src/main.ts
@@ -0,0 +1,58 @@
+import type { Actress } from "./types"
+
+function isActress(dati: unknown): dati is Actress {
+  if (
+    dati &&
+    typeof dati === "object" &&
+    "id" in dati &&
+    typeof dati.id === "number" &&
+    "name" in dati &&
+    typeof dati.name === "string" &&
+    "birth_year" in dati &&
+    typeof dati.birth_year === "number" &&
+    "biography" in dati &&
+    typeof dati.biography === "string" &&
+    "image" in dati &&
+    typeof dati.image === "string" &&
+    "awards" in dati &&
+    typeof dati.awards === "string" &&
+    "nationality" in dati &&
+    typeof dati.nationality === "string" &&
+    "most_famous_movies" in dati &&
+    Array.isArray(dati.most_famous_movies) &&
+    dati.most_famous_movies.length === 3 &&
+    dati.most_famous_movies.every(movie => typeof movie === "string")
+  ) {
+    return true
+  }
+  return false
+}
+
+async function getActress(id: number): Promise<Actress | null > {
+    try {
+        const response = await fetch(`http://localhost:3333/actresses/${id}`)
+
+    if (!response.ok) {
+      throw new Error(`Errore http ${response.status}: ${response.statusText}`)
+    }
+    const dati: unknown = await response.json()
+    if (!isActress(dati)) {
+      throw new Error('formato dei dati non valido')
+    }
+    console.log(dati);
+    return dati 
+  
+  } catch (errore) {
+    if (errore instanceof Error) {
+      console.error(`Errore durante il recupero dei dati`, errore.message)
+      
+    } else {
+      console.error('errore sconosciuto:', errore)
+    }
+    return null
+  }
+
+  
+}
+
+getActress(4)
